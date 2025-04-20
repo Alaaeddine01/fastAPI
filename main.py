a2ca44f33df59982daf6ac457cf4f270ca1e48dd
Alaaeddine elhaouch <alaaeddine958@gmail.com>
2025-04-20 13:34:12 +0100

    added some end points

--- main.py
+++ main.py
@@ -4,25 +4,18 @@ from books import BOOKS
 app = FastAPI()
 
 # uvicorn main:app --reload
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/api/{dynamic_param}")
-async def hello(dynamic_param):
-    return {'dynamic_param':dynamic_param}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-@app.get("/books")
+@app.get("/api/books")
 async def get_all_books():
     return BOOKS
-
-@app.get("/api")
-async def first_api():
-    return {"name":"alaa"}
+@app.get("/api/test/mybook")
+async def test_path_param():
+    return {'static_param':'static parameter !'}
+@app.get("/api/test/{dynamic_param}")
+async def test_path_param(dynamic_param):
+    return {'dynamic_param':dynamic_param}
+@app.get("/api/books/{book_title}")
+async def get_book_by_title(book_title:str):
+    for book in BOOKS:
+        if book.get('title').casefold()==book_title.casefold():
+            return book
 
