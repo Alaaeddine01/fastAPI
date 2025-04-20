66e23882e0e1f1b464f2ebbabe92e285e54f65c2
Alaaeddine elhaouch <alaaeddine958@gmail.com>
2025-04-20 13:33:16 +0100

    addded some end-points

---
+++ books.py
@@ -0,0 +1,10 @@
+
+
+BOOKS = [
+
+    {'title':'java_programming','author':'john','price':23.00},
+    {'title':'python_programming','author':'john','price':20.00},
+    {'title':'php_programming','author':'jack','price':23.00},
+    {'title':'go_programming','author':'roby','price':21.00},
+
+]
--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from fastapi import FastAPI
+from books import BOOKS
 
 app = FastAPI()
+
+# uvicorn main:app --reload
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/api/{dynamic_param}")
+async def hello(dynamic_param):
+    return {'dynamic_param':dynamic_param}
+
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+@app.get("/books")
+async def get_all_books():
+    return BOOKS
+
+@app.get("/api")
+async def first_api():
+    return {"name":"alaa"}
+
